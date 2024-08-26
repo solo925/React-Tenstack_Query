4af7f0032a915220722b8d040df9dc15420ebfc7
davinci <davinci@localhost.localdomain>
2024-08-26 13:09:14 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
 import './App.css';
+import { fetchListOfProducts } from './react-query';
 
 
 function App() {
+
+  const {data:productList,isLoading} = useQuery({
+    queryKey: ['productList'],
+    queryFn: () => fetchListOfProducts(),
+  });
+  // console.log(query);
+
+  if (isLoading) return <h1>Loading Products ! please wait</h1>
+  
 
 
   return (
     <div className="App">
       <h1>Say Hello</h1>
+      <ul>
+        {
+          productList?.length > 0 ? (
+            productList.map((product) => <li key={product.id}>{ product.title}</li>)
+          ):(<h1>No products found</h1>)
+        };
+      </ul>
 
      
     </div>
