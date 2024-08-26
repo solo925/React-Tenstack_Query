f80044154c7dba49798eb495b3c9db2484bcac38
davinci <davinci@localhost.localdomain>
2024-08-26 13:57:31 +0300

    getQuryClient and mutationFunction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
 import './App.css';
-import { fetchListOfProducts } from './react-query';
+import { addNewProduct, fetchListOfProducts } from './react-query';
 
 
 function App() {
+  const [productTitle, setProductTitle] = useState('')
+  const getQueryClient = useQueryClient();
 
-  const {data:productList,isLoading} = useQuery({
+  const { data: productList, isLoading } = useQuery({
     queryKey: ['productList'],
     queryFn: () => fetchListOfProducts(),
   });
   // console.log(query);
-
+  const {mutateAsync : handleAddnewProductMutation} = useMutation({
+    mutationFn: addNewProduct,
+    onSuccess: () => {
+      getQueryClient.invalidateQueries(['productList'])
+    }
+  });
+  
+  async function handleAddnewProduct() {
+    await handleAddnewProductMutation(productTitle)
+    setProductTitle('')
+    
+  }
+  
   if (isLoading) return <h1>Loading Products ! please wait</h1>
   
 
 
   return (
     <div className="App">
-      <h1>Say Hello</h1>
+      <h1>React QUery demo</h1>
+      <div>
+        <input name='name'
+          type='text' placeholder='Set product title'
+          value={productTitle}
+          onChange={(e) => setProductTitle(e.target.value)} />
+        <button onClick={handleAddnewProduct} disabled={productTitle.trim() === ''} type='button'>
+          Add new Item
+        </button>
+
+      </div>
       <ul>
         {
           productList?.length > 0 ? (
